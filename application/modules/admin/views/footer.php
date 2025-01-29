d69030837373d7169cc60f97d33f5f9df2c391f1
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-29 18:09:53 +0700

    Update footer.php

--- application/modules/admin/views/footer.php
+++ application/modules/admin/views/footer.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 <!-- Footer -->
 <footer class="footer pt-0">
@@ -13,7 +13,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
 </footer>
 </div>
-</div>
+</main>
 
 <!-- Argon Scripts -->
 <!-- Core -->
