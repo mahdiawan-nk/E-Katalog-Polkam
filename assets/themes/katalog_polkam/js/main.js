66e32c451560b021c35e4c7462917b7f0a3cfb70
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-02-03 21:29:36 +0700

    Update main.js

--- assets/themes/katalog_polkam/js/main.js
+++ assets/themes/katalog_polkam/js/main.js
@@ -95,21 +95,22 @@ AOS.init({
 	$("nav .dropdown").hover(
 		function () {
 			var $this = $(this);
+			console.log($this)
 			// 	 timer;
 			// clearTimeout(timer);
-			$this.addClass("show");
-			$this.find("> a").attr("aria-expanded", true);
-			// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-			$this.find(".dropdown-menu").addClass("show");
+			// $this.addClass("show");
+			// $this.find("> a").attr("aria-expanded", true);
+			$this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+			// $this.find(".dropdown-menu").addClass("show");
 		},
 		function () {
 			var $this = $(this);
 			// timer;
 			// timer = setTimeout(function(){
 			$this.removeClass("show");
-			$this.find("> a").attr("aria-expanded", false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find(".dropdown-menu").removeClass("show");
+			// $this.find("> a").attr("aria-expanded", false);
+			$this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+			// $this.find(".dropdown-menu").removeClass("show");
 			// }, 100);
 		}
 	);
@@ -255,7 +256,7 @@ AOS.init({
 			console.log("nice");
 		});
 	};
-	OnePageNav();
+	// OnePageNav();
 
 	// magnific popup
 	$(".image-popup").magnificPopup({
