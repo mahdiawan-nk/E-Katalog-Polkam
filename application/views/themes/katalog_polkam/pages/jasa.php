716a9ef3cd8ab3e184f27190c03681ee9e5271f6
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-02-03 21:10:26 +0700

    Update jasa.php

--- application/views/themes/katalog_polkam/pages/jasa.php
+++ application/views/themes/katalog_polkam/pages/jasa.php
@@ -134,37 +134,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                         <h3><a
                                 href="<?php echo site_url('shop/product/' . $product->id . '/' . $product->sku . '/'); ?>"><?php echo $product->name; ?></a>
                         </h3>
-                       <!--  <div class="d-flex" hidden>
-                            <div class="pricing">
-                                <p class="price">
-                                    <?php if ($product->current_discount > 0) : ?>
-                                    <span class="mr-2 price-dc">Rp
-                                        <?php echo format_rupiah($product->price); ?></span><span class="price-sale">Rp
-                                        <?php echo format_rupiah($product->price - $product->current_discount); ?></span>
-                                    <?php else : ?>
-                                    <span class="mr-2"><span class="price-sale">Rp
-                                            <?php echo format_rupiah($product->price - $product->current_discount); ?></span>
-                                        <?php endif; ?>
-                                </p>
-                            </div>
-                        </div> -->
                         Tersedia : <?php echo ($product->stock > 0) ? $product->stock .' '. $product->product_unit: '<span class="text-danger"><em>Stok habis</em></span>'; ?> 
-                                
-                       <!--  <div class="bottom-area d-flex px-3">
-                            <div class="m-auto d-flex">
-                                <a href="<?php echo site_url('shop/product/' . $product->id . '/' . $product->sku . '/'); ?>"
-                                    class="buy-now d-flex justify-content-center align-items-center text-center">
-                                    <span><i class="ion-ios-menu"></i></span>
-                                </a>
-                                <a href="#"
-                                    class="add-to-chart add-cart d-flex justify-content-center align-items-center mx-1"
-                                    data-sku="<?php echo $product->sku; ?>" data-name="<?php echo $product->name; ?>"
-                                    data-price="<?php echo ($product->current_discount > 0) ? ($product->price - $product->current_discount) : $product->price; ?>"
-                                    data-id="<?php echo $product->id; ?>">
-                                    <span><i class="ion-ios-cart"></i></span>
-                                </a>
-                            </div>
-                        </div> -->
                     </div>
                 </div>
             </div>
@@ -175,75 +145,3 @@ defined('BASEPATH') or exit('No direct script access allowed');
         </div>
     </div>
 </section>
-
-<!-- <section class="ftco-section img"
-    style="background-image: url(<?php echo get_theme_uri('images/background_03.jpg'); ?>);">
-    <div class="container">
-        <div class="row justify-content-end">
-            <div class="col-md-6 heading-section ftco-animate deal-of-the-day ftco-animate">
-                <span class="subheading">Produk dengan Harga Terbaik</span>
-                <h2 class="mb-4">Deal of the day</h2>
-                <p><?php echo $best_deal->description; ?></p>
-                <h3><a href="#"><?php echo $best_deal->name; ?></a></h3>
-                <span class="price">Rp <?php echo format_rupiah($best_deal->price); ?> <a href="#">sekarang hanya Rp
-                        <?php echo format_rupiah($best_deal->price - $best_deal->current_discount); ?></a></span>
-                <div id="timer" class="d-flex mt-5">
-                    <div class="time pl-3">
-                        <a href="#" class="btn btn-primary add-cart" data-sku="<?php echo $best_deal->sku; ?>"
-                            data-name="<?php echo $best_deal->name; ?>"
-                            data-price="<?php echo ($best_deal->current_discount > 0) ? ($best_deal->price - $best_deal->current_discount) : $best_deal->price; ?>"
-                            data-id="<?php echo $best_deal->id; ?>"><i class="ion-ios-cart"></i></a>
-                    </div>
-                    <div class="time pl-3">
-                        <a class="btn btn-info"
-                            href="<?php echo site_url('shop/product/' . $best_deal->id . '/' . $best_deal->sku . '/'); ?>"
-                            class="buy-now d-flex justify-content-center align-items-center text-center">
-                            <span><i class="ion-ios-menu text-white"></i></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
-
-<!-- <section class="ftco-section testimony-section">
-    <div class="container">
-        <div class="row justify-content-center mb-5 pb-3">
-            <div class="col-md-7 heading-section ftco-animate text-center">
-                <span class="subheading">Testimony</span>
-                <h2 class="mb-4">Apa yang pelanggan kami katakan?</h2>
-            </div>
-        </div>
-        <div class="row ftco-animate">
-            <div class="col-md-12">
-                <div class="carousel-testimony owl-carousel">
-                    <?php if (count($reviews) > 0) : ?>
-                    <?php foreach ($reviews as $review) : ?>
-                    <div class="item">
-                        <div class="testimony-wrap p-4 pb-5">
-                            <div class="user-img mb-5"
-                                style="background-image: url(<?php echo base_url('assets/uploads/users/' . $review->profile_picture); ?>)">
-
-
-
-                                <span class="quote d-flex align-items-center justify-content-center">
-                                    <i class="icon-quote-left"></i>
-                                </span>
-                            </div>
-                            <div class="text text-center">
-                                <p class="mb-5 pl-4 line"><?php echo $review->review_text; ?></p>
-                                <p class="name"><?php echo $review->name; ?></p>
-                                <span class="position"><?php echo get_formatted_date($review->review_date); ?></span>
-                            </div>
-                        </div>
-                    </div>
-                    <?php endforeach; ?>
-                    <?php else : ?>
-                    <?php endif; ?>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
